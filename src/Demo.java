ae5ddd35371b7ec6e3c0071d8b5927cb23d571be
Thesin-01 <sithumishra76@gmail.com>
2025-12-19 12:58:32 +0530

    commited

---
+++ src/Demo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Box {
+    double length, width, height;
+    double volume() {
+        return length * width * height;
+    }
+}
+public class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Box b = new Box();
+        System.out.print("Enter length: ");
+        b.length = sc.nextDouble();
+        System.out.print("Enter width: ");
+        b.width = sc.nextDouble();
+        System.out.print("Enter height: ");
+        b.height = sc.nextDouble();
+        System.out.println("Volume of the box = " + b.volume());
+        sc.close();
+    }
+}
+
